c0ec43e9497f44503ce97684fdfc3b840fa1b481
Hasan Tariq <hkhankor@gmail.com>
2024-07-31 01:25:47 +0500

    6th  commit

--- src/lib/wisp.ts
+++ src/lib/wisp.ts
@@ -1,3 +1,4 @@
+// Import the configuration and Wisp CMS client components
 import { config } from "@/config";
 import {
   buildWispClient,
@@ -5,8 +6,15 @@ import {
   GetPostResult,
 } from "@wisp-cms/client";
 
+// Check if blogId is defined before creating the client
+if (!config.wisp.blogId) {
+  throw new Error('Blog ID is not defined in the configuration.');
+}
+
+// Create a Wisp client instance using the blogId from the configuration
 export const wisp = buildWispClient({
   blogId: config.wisp.blogId,
 });
 
+// Export the types for use in other parts of your application
 export type { GetPostsResult, GetPostResult };
